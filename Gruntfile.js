ab73c6b0914c037928bf3cf533c80e80397bc9d2
Christopher Hiller <chiller@badwing.com>
2015-03-30 15:05:31 -0700

    add grunt-gh-pages, load-grunt-config and fix demo
    - `grunt-gh-pages` will publish demo.  execute `grunt publish-demo`.  it will build the demo from the current bui source then push the results to the `gh-pages` branch on github, automatically publishing the [demo](http://decipherinc.github.io/bui)
    - `load-grunt-config` allows separating tasks into multiple files, which is convenient, since we were getting many tasks
    - demo now uses cdns for 3p deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,106 +2,6 @@
 
 module.exports = function (grunt) {
 
-  var pkg = require('./package.json'),
-    BANNER = '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %> - (c) 2014-2015 ' +
-      'FocusVision Worldwide - Licensed MIT */\n\n';
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: pkg,
-    less: {
-      demo: {
-        files: {
-          'demo/css/demo-page-styles.css': 'demo/less/demo-' +
-          'page-specific.less'
-        }
-      },
-      main: {
-        options: {
-          banner: BANNER
-        },
-        files: {
-          'dist/bui.css': 'less/bui.less'
-        }
-      },
-      min: {
-        options: {
-          compress: true,
-          banner: BANNER
-        },
-        files: {
-          'dist/bui.min.css': 'less/bui.less'
-        }
-      }
-    },
-    watch: {
-      demo: {
-        files: ['demo/less/demo-page-specific.less', 'less/*.less'],
-        tasks: ['less:main', 'less:demo']
-      },
-      main: {
-        files: ['less/*.less'],
-        tasks: ['less:main']
-      }
-    },
-    devUpdate: {
-      options: {
-        updateType: 'prompt',
-        semver: false
-      },
-      main: {}
-    },
-    bump: {
-      options: {
-        files: [
-          'package.json',
-          'bower.json'
-        ],
-        updateConfigs: ['pkg'],
-        commitMessage: 'Release v%VERSION%',
-        commitFiles: [
-          'package.json',
-          'bower.json',
-          'dist/*'
-        ],
-        createTag: true,
-        tagName: 'v%VERSION%',
-        tagMessage: 'Version %VERSION%',
-        push: false,
-        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
-      }
-    },
-    clean: {
-      demo: 'demo/support/'
-    },
-    copy: {
-      demo: {
-        files: [
-          {
-            cwd: 'node_modules/bootstrap/dist/',
-            src: 'css/**',
-            dest: 'demo/support/bootstrap/',
-            expand: true
-          },
-          {
-            src: '**',
-            cwd: 'dist/',
-            dest: 'demo/support/bui/',
-            expand: true
-          }
-        ]
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-dev-update');
-  grunt.loadNpmTasks('grunt-bump');
-
   grunt.registerTask('release', 'Bump version & release', function (target) {
     if (!target) {
       return grunt.fail.warn('Unable to release without valid target.  ' +
@@ -112,18 +12,14 @@ module.exports = function (grunt) {
     grunt.task.run('bump-commit');
   });
 
-  grunt.registerTask('default', 'Compile LESS', ['build']);
-  grunt.registerTask('demo', 'Build demo', [
-    'build',
-    'less:demo',
-    'clean:demo',
-    'copy:demo'
-  ]);
-  grunt.registerTask('build', 'Compile LESS', [
-    'less:main',
-    'less:min'
-  ]);
-  grunt.registerTask('watch-demo',
-    'Watch demo LESS file(s) and recompile upon change', ['watch:demo']);
+  require('load-grunt-config')(grunt, {
+    configPath: 'tasks',
+    data: {
+      pkg: grunt.file.readJSON('package.json'),
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> - (c) 2014-2015 ' +
+        'FocusVision Worldwide - Licensed <%= pkg.license %> */\n\n'
+    }
+  });
 
 };
